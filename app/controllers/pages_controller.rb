a47dddedcaf2bb71557fdcc65fbd9973249ca0f7
sara <sara@nanomia.cl>
2016-10-18 19:20:11 -0300

    se sumo superman al desafio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
 class PagesController < ApplicationController
-  def batman
+  def superman
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/superman'
+
   get 'pages/batman'
 
   root 'pages#batman'
