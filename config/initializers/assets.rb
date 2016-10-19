62e6b927cc8fcdb8fe09e5f87aeb41266eaa53e8
sara <sara@nanomia.cl>
2016-10-18 21:16:01 -0300

    incluir boostraps pag2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,10 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.paths << Rails.root.join ('vendor', 'assets', 'images')
-
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'ccs')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'js')
 
 
 
@@ -13,4 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join ('vendor', 'assets', 'i
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( jpg )
+Rails.application.config.assets.precompile += %w( *.png *.jpg )
